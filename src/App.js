43b15dbc69f3143902088adc7f0686cb91073e47
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-03-20 11:30:09 -0500

    initial app setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;

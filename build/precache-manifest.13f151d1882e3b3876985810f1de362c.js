4a280a256e521607ac7e6a7e3fa812c5ae88a7d4
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-03-21 13:04:31 -0500

    build folder

---
+++ build/precache-manifest.13f151d1882e3b3876985810f1de362c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278c1ece62ee640abf45af1cd97d5703",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69f46192dcd67a3b046d",
+    "url": "/static/css/main.d55ebedd.chunk.css"
+  },
+  {
+    "revision": "868adfec63092d5012f3",
+    "url": "/static/js/2.caa11aab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.caa11aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f46192dcd67a3b046d",
+    "url": "/static/js/main.9f8943c0.chunk.js"
+  },
+  {
+    "revision": "6e47440f006e44186a33",
+    "url": "/static/js/runtime-main.a3e8823f.js"
+  },
+  {
+    "revision": "e4582b14c57545b36930e6187dffb183",
+    "url": "/static/media/buffalo.e4582b14.jpg"
+  },
+  {
+    "revision": "bb6e94645249c7a5cd41567a5391858e",
+    "url": "/static/media/burger.bb6e9464.png"
+  },
+  {
+    "revision": "1a37a1694d5c64ded51aa972a99fa81e",
+    "url": "/static/media/me.1a37a169.jpg"
+  },
+  {
+    "revision": "21933e4237ae7a21dab43545b4aa2e91",
+    "url": "/static/media/music.21933e42.jpg"
+  },
+  {
+    "revision": "95ef3d5deaedee2c70d46f5789b8b3a1",
+    "url": "/static/media/weather.95ef3d5d.png"
+  }
+]);

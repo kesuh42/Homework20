a4e707d3de68a6747cfb04c3a1d27641fdafb910
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-03-21 13:30:19 -0500

    Updates

---
+++ precache-manifest.fc5e50da52225b8460f6a020a34d5a01.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4386dfcd354e169a4be73dcfe62d4cba",
+    "url": "/Homework20/index.html"
+  },
+  {
+    "revision": "be3858c0da844b5a5e69",
+    "url": "/Homework20/static/css/main.d55ebedd.chunk.css"
+  },
+  {
+    "revision": "868adfec63092d5012f3",
+    "url": "/Homework20/static/js/2.caa11aab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Homework20/static/js/2.caa11aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3858c0da844b5a5e69",
+    "url": "/Homework20/static/js/main.275740c6.chunk.js"
+  },
+  {
+    "revision": "34beb4fbd442f4ebd037",
+    "url": "/Homework20/static/js/runtime-main.8bc70c52.js"
+  },
+  {
+    "revision": "e4582b14c57545b36930e6187dffb183",
+    "url": "/Homework20/static/media/buffalo.e4582b14.jpg"
+  },
+  {
+    "revision": "bb6e94645249c7a5cd41567a5391858e",
+    "url": "/Homework20/static/media/burger.bb6e9464.png"
+  },
+  {
+    "revision": "1a37a1694d5c64ded51aa972a99fa81e",
+    "url": "/Homework20/static/media/me.1a37a169.jpg"
+  },
+  {
+    "revision": "21933e4237ae7a21dab43545b4aa2e91",
+    "url": "/Homework20/static/media/music.21933e42.jpg"
+  },
+  {
+    "revision": "95ef3d5deaedee2c70d46f5789b8b3a1",
+    "url": "/Homework20/static/media/weather.95ef3d5d.png"
+  }
+]);
